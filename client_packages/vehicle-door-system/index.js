77b203dc5dd384faf7546aa4143b3441e6e9fdc8
Alberts Matrozis <62105039+LurkErLV@users.noreply.github.com>
2022-10-31 02:28:15 +0200

    Update index.js

--- client_packages/vehicle-door-system/index.js
+++ client_packages/vehicle-door-system/index.js
@@ -1,6 +1,6 @@
 mp.keys.bind(0x4C, true, () => {
     let localPlayer = mp.players.local
-    let idVehicle = mp.game.vehicle.getClosestVehicle(localPlayer.position.x, localPlayer.position.y, localPlayer.position.z, 10, 0, 70)
+    let idVehicle = mp.game.vehicle.getClosestVehicle(localPlayer.position.x, localPlayer.position.y, localPlayer.position.z, 150, 0, 70)
     let vehicle = mp.vehicles.atHandle(idVehicle)
     if (!vehicle) return;
     
